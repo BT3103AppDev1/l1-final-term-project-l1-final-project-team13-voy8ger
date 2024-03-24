1d232027b8337c0cbb6cd5532d34ee062c05104b
ramanen <e0958216@u.nus.edu>
2024-03-24 15:18:03 +0800

    edited Auth,issues with google Auth

--- src/views/Auth/LogIn.vue
+++ src/views/Auth/LogIn.vue
@@ -1,44 +1,123 @@
+<script setup>
+import { onMounted } from "vue";
+
+// example components
+import Header from "@/components/Header.vue";
+
+//Vue Material Kit 2 components
+import MaterialInput from "@/components/material_components/MaterialInput.vue";
+import MaterialSwitch from "@/components/material_components/MaterialSwitch.vue";
+import MaterialButton from "@/components/material_components/MaterialButton.vue";
+
+// material-input
+import setMaterialInput from "@/assets/js/material-input";
+onMounted(() => {
+  setMaterialInput();
+});
+</script>
 <template>
-  <body>
-    <div class="wrapper">
-      <form action="">
-        <div class="boxer">
-          <h1>Welcome Back</h1>
-        </div>
+  <Header>
+    <div
+      class="page-header align-items-start min-vh-100"
+      :style="{
+        backgroundImage:
+          'url(https://images.unsplash.com/photo-1497294815431-9365093b7331?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1950&q=80)'
+      }"
+      loading="lazy"
+    >
+      <span class="mask bg-gradient-dark opacity-6"></span>
+      <div class="container my-auto">
+        <div class="row">
+          <div class="col-lg-4 col-md-8 col-12 mx-auto">
+            <div class="card z-index-0 fadeIn3 fadeInBottom">
+              <div
+                class="card-header p-0 position-relative mt-n4 mx-3 z-index-2"
+              >
+                <div
+                  class="bg-gradient-success shadow-success border-radius-lg py-3 pe-1"
+                >
+                  <h4
+                    class="text-white font-weight-bolder text-center mt-2 mb-0"
+                  >
+                    Welcome Back
+                  </h4>
+                  <div class="row mt-3 justify-content-center">
+                    
+                    <div class="col-2 text-center mm-auto bg-black-hover">
+                      <a class="btn btn-link px-2" href="javascript:;">
+                        <i class="fa fa-google text-white text-lg" v-on:click="googleSignIn"></i>
+                      </a>
+                    </div>
 
-        <!-- All the form elements -->
+                  </div>
+                </div>
+              </div>
+              <div class="card-body">
+                <form role="form" class="text-start">
+                  <MaterialInput
+                    id="email"
+                    class="input-group-outline my-3"
+                    :label="{ text: 'Email', class: 'form-label' }"
+                    type="email"
+                  />
+                  <MaterialInput
+                    id="password"
+                    class="input-group-outline mb-3"
+                    :label="{ text: 'Password', class: 'form-label' }"
+                    type="password"
+                  />
+                  <MaterialSwitch
+                    class="d-flex align-items-center mb-3"
+                    id="rememberMe"
+                    labelClass="mb-0 ms-3"
+                    checked
+                    >Remember me</MaterialSwitch
+                  >
 
-        <div class="input-box">
-          <input type="text" placeholder="Email" v-model="email" required />
+                  <div class="text-center">
+                    <MaterialButton
+                      class="my-4 mb-2"
+                      variant="gradient"
+                      color="success"
+                      fullWidth
+                      v-on:click.prevent="login"
+                      >Sign in</MaterialButton
+                    >
+                  </div class = "register-link">
+                  <p class="mt-4 text-sm text-center">
+                    Don't have an account?
+                    <a
+                      href="#"
+                      class="text-success text-gradient font-weight-bold"
+                      v-on:click="pusher"
+                      >Sign up</a
+                    >
+                  </p>
+                  <p class="mt-1 text-sm text-center">
+                    Forgot Password?
+                    <a
+                      href="#"
+                      class="text-success text-gradient font-weight-bold"
+                      v-on:click="resetPassword"
+                      >Reset</a
+                    >
+                  </p>
+                </form>
+                
+              </div>
+            </div>
+          </div>
         </div>
-
-        <div class="input-box">
-          <input
-            type="password"
-            placeholder="Password"
-            v-model="password"
-            required
-          />
-        </div>
-
-        <button type="submit" class="btn" v-on:click.prevent="login">
-          Log In
-        </button>
-        <div class="register-link">
-          <p>
-            Don't have an account? <a href="#" v-on:click="pusher">Sign up</a>
-          </p>
-          <p>Forgot Password? <a href="#">Reset</a></p>
-        </div>
-      </form>
+      </div>
+      
     </div>
-  </body>
+  </Header>
 </template>
 
 <script>
 import { app } from "../../firebase.js"; // Import your Firebase app instance
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-// import { ref } from "vue";
+import { collection, doc, setDoc } from "firebase/firestore"; 
+import { getAuth, signInWithEmailAndPassword, signInWithPopup, sendPasswordResetEmail, GoogleAuthProvider } from "firebase/auth";
 
 export default {
   // get required data if any
@@ -58,20 +137,33 @@ export default {
     pusher() {
       this.$router.push({ name: "SignUp" });
     },
-    // login User
+
+    // login user with google
+    async googleSignIn() {
+
+      const auth = getAuth(app);
+      const provider = new GoogleAuthProvider();
+      await signInWithPopup(auth, provider);
+    },
+
+    // login User with emai
     async login() {
+      // since cannot pass in v-model directly
+      this.email = document.getElementById("email").value;
+      this.password = document.getElementById("password").value 
+
       try {
         // create object for authentication
         const auth = getAuth(app);
 
         // error handling of passwords
-        if (this.password.length == 0) {
-          this.error = "Password Field is empty";
-          throw this.error;
-        } else if (this.email.length == 0) {
+        if (this.email.length == 0) {
           this.error = "Email Field is empty";
           throw this.error;
-        }
+        } else if (this.password.length == 0) {
+          this.error = "Password Field is empty";
+          throw this.error;
+        } 
         // if no errors/things to flag in user input
         else {
           await signInWithEmailAndPassword(auth, this.email, this.password)
@@ -106,121 +198,44 @@ export default {
         }
       }
     },
+
+    // reset password - only for email
+    async resetPassword(email) {
+      const auth = getAuth(app);
+      // since cannot pass in v-model directly
+      this.email = document.getElementById("email").value;
+      this.password = document.getElementById("password").value 
+
+      await sendPasswordResetEmail(auth,this.email)
+        .then(() => {
+          // Password reset email sent.
+          this.$toast.success("Password reset email sent successfully.");
+        })
+        .catch((error) => {
+          // Error occurred. Handle error here.
+          if (this.email.length == 0) {
+            this.$toast.error("Please key in an email to reset the password");
+          } else {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log(error);
+            this.$toast.error("Error sending email to reset password");
+          }
+        });
+    }
   },
 };
 </script>
 
 <style scoped>
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  font-family: "Poppins", sans-serif;
-}
-body {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  min-height: 100vh;
-  background: url(bg.jpg) no-repeat;
-  background-size: cover;
-  background-position: center;
-  background-color: #333;
+.bg-black-hover {
+    transition: 0.3s ease;
 }
 
-.wrapper {
-  width: 400px;
-  border: 2px solid rgba(255, 255, 255, 0.2);
-  backdrop-filter: blur(9px);
-  background-color: #fff;
-  border-radius: 10px;
-  padding: 30px 40px;
+.bg-black-hover:hover {
+    background-color: darkgreen;
 }
 
-.wrapper .boxer {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  height: 55px;
-  background: linear-gradient(to bottom right, turquoise, blue);
-  box-shadow: 0px 5px 5px -5px rgba(0, 0, 0, 0.75);
-  border-radius: 10px;
-}
-
-.boxer h1 {
-  font-size: 20px;
-  color: white;
-}
-
-.wrapper .input-box {
-  position: relative;
-  width: 100%;
-  height: 35px;
-  margin: 10px 0;
-}
-
-.input-box input {
-  width: 100%;
-  height: 100%;
-  border: 1px solid lightslategray;
-  border-radius: 5px;
-  font-size: 13px;
-  color: black;
-  padding: 15px 40px 15px 15px;
-}
-
-.input-box input::placeholder {
-  color: lightslategray;
-}
-
-.wrapper .remember-forgot {
-  display: inline-block;
-  justify-content: space-between;
-  font-size: 14.5px;
-  margin: 10px 0px 15px;
-}
-.remember-forgot label input {
-  accent-color: #fff;
-  margin-right: 5px;
-}
-.remember-forgot a {
-  color: #fff;
-  text-decoration: none;
-}
-
-.remember-forgot a:hover {
-  text-decoration: underline;
-}
-
-.wrapper .btn {
-  width: 100%;
-  height: 45px;
-  border: none;
-  background: turquoise;
-  border-radius: 10px;
-  font-size: 16px;
-  color: white;
-  font-weight: 600;
-}
-
-.wrapper .btn:hover {
-  filter: brightness(85%);
-}
-
-.wrapper .register-link {
-  font-size: 14.5px;
-  text-align: center;
-  margin: 20px 3px 15px;
-  color: lightslategray;
-}
-.register-link p a {
-  color: black;
-  text-decoration: none;
-  font-weight: 600;
-}
-
-.register-link p a:hover {
-  text-decoration: underline;
-}
 </style>
+
+
