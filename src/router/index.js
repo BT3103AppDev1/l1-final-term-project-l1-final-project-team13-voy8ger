724b7f79fe01f56c99bc2f915bf2e227c03bea47
Doris Lai <doris.lai58@gmail.com>
2024-03-28 13:39:10 +0800

    Update index.js
    add explore and create

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,9 @@ import SignUp from "../views/Auth/SignUp.vue";
 import LogIn from "../views/Auth/LogIn.vue";
 import Home from "../views/Home.vue";
 import Profile from "../views/Profile.vue";
+import Explore from "../views/Explore.vue";
+//import Create from "../views/Create.vue";
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -31,6 +34,22 @@ const router = createRouter({
       path: "/",
       name: "Home",
       component: Home,
+    },
+    {
+      path: "/explore",
+      name: "Explore",
+      component: Explore,
+    },
+    /*
+    {
+      path: "/create",
+      name: "Create",
+      component: Create,
+    },
+    */
+    {
+      path: "/",
+      redirect: "/home" // Redirect the root path to Home
     },
   ],
 });
