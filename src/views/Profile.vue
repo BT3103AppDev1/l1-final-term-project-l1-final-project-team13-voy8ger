331cf94d46e063112701974196c1a9bbb2d794c2
jeronlxj <jeronliaw@u.nus.edu>
2024-04-24 03:46:25 +0800

    prfle cards + likes

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -39,47 +39,42 @@
         <p v-if="user.bio != null">{{ user.bio }}</p>
         <v-btn class="w-50 mb-5" @click="goEdit">Edit Profile</v-btn>
         <v-row>
-          <v-col v-for="output in filterer" :key="output.planId" cols="4">
-            <v-card
-              class="mx-auto card"
-              max-width="300"
-              max-height="250"
-              @click="goToUpdatePage(output.planId)"
+          <v-col v-for="output in filterer" cols="4"> 
+            <v-card class="mx-auto card" max-width="330" max-height="250" v-on:click = "goToUpdatePage(output.planId)">
+
+            <v-img
+            class="align-end text-white"
+            height="150"
+            src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+            cover
             >
-              <v-img
-                height="150px"
-                src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-                cover
-              ></v-img>
-              <v-card-title>{{ output.Plan_Name }}</v-card-title>
-              <v-row align="center">
+
+            <v-card-title >{{ output.Plan_Name }}</v-card-title>
+            </v-img>
+
+            <v-row align="center">
                 <v-col cols="6">
-                  <v-card-text>{{ output.num_likes }} likes</v-card-text>
-                  <v-card-subtitle>{{
-                    output.plan_description
-                  }}</v-card-subtitle>
+                    <v-card-text>
+                    <div>{{ output.likeCount }} likes</div>
+                    <div class="truncate">{{ output.plan_description }}</div>
+                    </v-card-text>
                 </v-col>
+
                 <v-col cols="6">
-                  <v-card-actions>
+                    <v-card-actions>
                     <v-spacer></v-spacer>
-                    <v-btn
-                      color="error"
-                      icon
-                      size="small"
-                      variant="plain"
-                      @click.stop="toggleHeart(output.planId)"
-                    >
-                      <v-icon>{{
-                        user.saved.includes(output.planId)
-                          ? "mdi-heart"
-                          : "mdi-heart-outline"
-                      }}</v-icon>
+                    <v-btn color="error" icon size="small" variant="plain" @click.stop="toggleHeart(output.planId)">
+                        <v-icon>{{ user.saved.includes(output.planId) ? 'mdi-heart' : 'mdi-heart-outline' }}</v-icon>
                     </v-btn>
-                  </v-card-actions>
+
+                    <v-btn color="#0077B6" icon small variant="plain" size="small" @click.stop="toggleLike(output.planId)">
+                      <v-icon>{{output.AllowLike ? 'mdi-thumb-up-outline':'mdi-thumb-up'}}</v-icon>
+                    </v-btn>
+                    </v-card-actions>
                 </v-col>
-              </v-row>
-            </v-card>
-          </v-col>
+            </v-row>
+          </v-card>
+        </v-col>
         </v-row>
       </div>
     </v-container>
@@ -114,7 +109,7 @@ export default {
     return {
       user: {
         id: "",
-        name: "John Doe",
+        name: "loading name...",
         email: "",
         profilePic: "https://cdn.vuetifyjs.com/images/john.jpg",
         bio: null,
@@ -150,9 +145,6 @@ export default {
   },
 
   methods: {
-    goToUpdatePage(planId) {
-      this.$router.push({ name: "UpdatePlan", params: { planId } });
-    },
     async toggleHeart(planId) {
       // get document of that user
       const docRef = doc(db, "Users", String(this.user.email));
@@ -166,9 +158,26 @@ export default {
           saved_list: [...this.user.saved, planId],
         });
       }
-      // refresh
-      this.user.plans = [];
       this.fetchAndUpdateData(String(this.user.email));
+    },
+
+    async toggleLike(plan_Name) {
+      // get document of that liked users from collection Likes
+      const docRef = doc(db, "Likes", plan_Name);
+      const docSnap = await getDoc(docRef);
+
+      if(docSnap.data().Liked_Users.includes(String(this.user.email))) {
+        // remove users from liked 
+        await this.deleteListItem(docRef, docSnap, "Liked_Users", String(this.user.email));
+
+        this.fetchAndUpdateData(this.user.email);
+
+      } else {
+        // add users to liked
+        await this.addListItem(docRef, docSnap, "Liked_Users", String(this.user.email));
+
+        this.fetchAndUpdateData(this.user.email);
+      }
     },
 
     async deleteListItem(docRef, docSnap, listFieldName, itemToRemove) {
@@ -194,6 +203,28 @@ export default {
       }
     },
 
+    async addListItem(docRef, docSnap, listFieldName, itemToAdd) {
+      try {
+        if (docSnap.exists()) {
+          // Get the data from the document
+          const existingList = docSnap.data()[listFieldName];
+          
+          // Add the new item to the existing list
+          existingList.push(itemToAdd);
+
+          // Update the document with the modified list
+          await updateDoc(docRef, {
+            "Liked_Users": existingList,
+          });
+          
+        } else {
+          console.log("Document does not exist!");
+        }
+      } catch (error) {
+        console.error("Error adding item to list:", error);
+      }
+    },
+
     async fetchAndUpdateData(userEmail) {
       // get created plan list of the user
       const docSnap = await getDoc(doc(db, "Users", String(userEmail)));
@@ -213,17 +244,49 @@ export default {
       console.log("created: " + docSnap.data().plans_list);
       console.log("saved: " + docSnap.data().saved_list);
 
-      // get all the plans out & put it into the list temp
+      // refresh
+      this.user.plans = [];
+      // get all the plans out & put it into user.plans
       const val = docSnap.data().plans_list.map(async (e) => {
-        this.user.plans.push((await getDoc(doc(db, "Plans", e))).data());
+        let tempLike = true;
+
+        // get data if the user has liked this or not
+        const docRef3 = doc(db, "Likes", e);
+        const docSnap3 = await getDoc(docRef3);
+        for(let i = 0; i < docSnap3.data().Liked_Users.length; i++) {
+        
+          // user has liked this so dont let him like again
+          if(docSnap3.data().Liked_Users[i] == this.user.email) {
+            tempLike = false;
+          }          
+        } 
+        let likeCount = await this.getNumLikes(e);
+
+        // along with plan details pass in if user has liked this or not
+        let deets = (await getDoc(doc(db, "Plans", e))).data();
+        deets.AllowLike = tempLike
+        deets.likeCount = likeCount;
+
+        this.user.plans.push(deets);
       });
       this.user.saved = docSnap.data().saved_list;
 
       console.log("created temp: " + this.user.plans);
       console.log("saved temp: " + this.user.saved);
     },
+    async getNumLikes(planId) {
+      const docRef = doc(db, "Likes", planId);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        this.likeCount = docSnap.data().Liked_Users.length;
+        return this.likeCount;
+      }
+    },
     goEdit() {
       this.$router.push("/editprfle");
+    },
+    goToUpdatePage(planId) {
+      this.$router.push({ name: "UpdatePlan", params: { planId } });
     },
   },
 };
