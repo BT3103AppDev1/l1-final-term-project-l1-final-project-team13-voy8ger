a436b4948be3dfcfb08fc7e583925a2e7854ab84
Coco Li <cocoli413S@gmail.com>
2024-04-24 11:16:50 +0800

    Added Spending data to Analytics

--- src/views/Analytics.vue
+++ src/views/Analytics.vue
@@ -5,13 +5,32 @@
       <v-container>
         <v-row class="mt-15" justify="center">
           <h1 class="text-black pt-3 mt-n5 me-2 text-center">Analytics</h1>
-          <!-- Render the BarChart only when dataReady is true -->
-          <BarChart :chart-data-external="chartData" v-if="chartDataReady" />
+        </v-row>
+          <v-row class="mt-15" justify="center">
+          <v-col cols="5">
+            <br>
+            <h3 class="text-black pt-3 mt-n5 me-2 text-center">
+              
+            Plans Created: {{ plansCreated }}
+          </h3>
+          </v-col>
+          <v-col cols="1">
+            <div class="divider"></div> <!-- Add a divider -->
+          </v-col>
+          <v-col cols="1">
+            
+          </v-col>
+          <v-col cols="5" justify="center" align="center">
+            <br>
+            <h3 class="text-black pt-3 mt-n5 me-2 text-center" >
+            Total Spending: ${{ chartData.thisMonth + chartData.lastMonth }}
+          </h3>
+          </v-col>
         </v-row>
         <v-row class="mt-15" justify="center">
-          <h1 class="text-black pt-3 mt-n5 me-2 text-center">
-            Plans Created: {{ plansCreated }}
-          </h1>
+          
+          <!-- Render the BarChart only when dataReady is true -->
+          <BarChart :chart-data-external="chartData" v-if="chartDataReady" />
         </v-row>
       </v-container>
     </div>
@@ -28,6 +47,7 @@ import { db } from "../firebase.js";
 import { doc, getDoc } from "firebase/firestore";
 import { ref, reactive, onMounted } from "vue";
 
+// get the user
 const auth = getAuth();
 const user = ref(null);
 const userEmail = ref("");
@@ -38,7 +58,7 @@ const chartData = reactive({
 });
 const chartDataReady = ref(false);
 const plansCreated = ref(0);
-
+// access only if authenticated
 onAuthStateChanged(auth, (userInstance) => {
   if (userInstance) {
     user.value = userInstance;
@@ -46,7 +66,7 @@ onAuthStateChanged(auth, (userInstance) => {
     fetchPlans();
   }
 });
-
+// fetch the plans created by the user
 async function fetchPlans() {
   const docRef = doc(db, "Users", userEmail.value);
   const docSnap = await getDoc(docRef);
@@ -56,7 +76,7 @@ async function fetchPlans() {
     await aggregateSpending(plansList);
   }
 }
-
+// get the spending for each plan
 async function aggregateSpending(plansList) {
   const today = new Date();
   const lastMonth = new Date(today.getFullYear(), today.getMonth() - 1, 1);
@@ -98,3 +118,11 @@ export default {
   },
 };
 </script>
+<style scoped>
+/* Add styling for the divider */
+.divider {
+  border-right: 1px solid #ccc;
+  height: 100px; /* Adjust the height as needed */
+  margin: 0 auto;
+}
+</style>
